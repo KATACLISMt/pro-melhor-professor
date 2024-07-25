1b11e640f08933cd42cd77a6cc7adb400326833a
KATACLISMt <moritylicien@gmail.com>
2024-07-25 20:13:55 +0000

    vamos la

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home(){
+    return(
+        <div>
+            <h1>Muito obrigado pelo seu tempo</h1>
+                <p>estou adorando</p>
+                    <p>me sinto muito gruta</p>
+             <img src="https://external-preview.redd.it/oPDLQXls4_bT3MBWwUl95V7JnYP7YBs7ofHESFM9-Ic.jpg?auto=webp&s=0a1145aa43cfbfc9d9f61be58888645a9feeb7da"/>
+ <p>segue um video do meu cerebro no momento</p>
+
+<a href="https://www.youtube.com/watch?v=6AaDFgdJLUw&ab_channel=WarFighters"> BEM AQUI</a>
+        </div>
+    )
+}
+export default Home
